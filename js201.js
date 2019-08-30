f28d01f0518588eead5ade69838e1c2a578fe119
Dominoeffekten <31511825+Dominoeffekten@users.noreply.github.com>
2019-08-30 15:56:33 +0200

    Update js201.js

--- js201.js
+++ js201.js
@@ -2,7 +2,5 @@
 
 var year = Number(prompt('Which year is this?'));
 
-console.log('This is a leapyear ' + year%4===0 && year*4===0);
+console.log('This is a leap year: ' + (year % 4 === 0) || (year % 100 === 0) || (year % 400 === 0));
 
-
-skud år 2020
