b69f9f796c63f95e6ab6416e8d8765cd7ce59dbf
Dominoeffekten <31511825+Dominoeffekten@users.noreply.github.com>
2019-08-30 13:27:21 +0200

    Create js15.js

---
+++ js15.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var fahrenheit = Number(prompt('Enter the temperature in Fahrenheit'));
+
+var celsius = 5 / 9 * (fahrenheit - 32);
+
+console.log('The temperature in Celsius is ' + celsius);
