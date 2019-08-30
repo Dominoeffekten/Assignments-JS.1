82e95db3815d8c02852a6fa9785d07ab2598d01a
Dominoeffekten <31511825+Dominoeffekten@users.noreply.github.com>
2019-08-30 16:37:24 +0200

    Update js25c.js

--- js25c.js
+++ js25c.js
@@ -3,6 +3,7 @@
 console.log('Truth Table Conjunction');
 
 console.log('var1\tvar2\tvar1 && var2');
+
 var var1 = true;
 var var2 = true;
 console.log(var1 + '\t' + var2 + '\t' + 'true'); // true, true, true
