2773118ce798993be4c45f2c912684321be5ef5f
Dominoeffekten <31511825+Dominoeffekten@users.noreply.github.com>
2019-08-30 16:33:39 +0200

    Create js25d.js

---
+++ js25d.js
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log('Truth Table Disjunction');
+console.log('var1\tvar2\tvar1 && var2');
+
+var var1 = true;
+var var2 = true;
+console.log(var1 + '\t' + var2 + '\t' + 'true'); //true, true, true
+
+var var1 = true;
+var var2 = false;
+console.log(var1 + '/t' + var2 + \t + 'true'); //true, false, true
+
+var var1 = false;
+var var2 = true;
+console.log();
